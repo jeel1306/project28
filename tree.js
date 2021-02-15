2ed46458a3a845bb977608f1405353bf6a6af683
jeel1306 <72064603+jeel1306@users.noreply.github.com>
2021-02-15 11:14:35 +0530

    Add files via upload

---
+++ tree.js
@@ -0,0 +1,37 @@
+class tree
+{
+    constructor(x,y)
+{
+    this.x=x;
+    this.y=y;
+    this.width=450;
+    this.height=600
+    this.wallThickness=10;
+
+    this.image=loadImage("images/tree.png")
+    this.bottomBody=Bodies.rectangle(this.x,this.y,this.width,this.wallThickness,{isStatic:true});
+    this.leftWallBody=Bodies.rectangle(0,this.y-this.height/2,this.wallThickness,this.height,{isStatic:false});
+    this.rightWallBody=Bodies.rectangle(this.x+this.width,this.y-this.height/2,this.wallThickness,this.height,{isStatic:false});
+    
+    World.add(world,this.bottomBody);
+    World.add(world,this.leftWallBody);
+    World.add(world,this.rightWallBody);
+
+}
+  display()
+  {
+       var posBottom=this.bottomBody.position;
+       push();
+       translate(posBottom.x,posBottom.y+10);
+       fill(255)
+	   imageMode(CENTER);
+	   image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+	   pop()
+			
+
+  }
+  
+
+
+  
+}
